30930be986d5d57faf7cea9117f9d210229429ab
NikLeberg <niklaus.leuenb+github@gmail.com>
2021-07-15 21:07:47 +0000

    platform: basic esp32 support (untested)

---
+++ platform/esp32/platform.c
@@ -0,0 +1,68 @@
+/**
+ * @file platform.c
+ * @author NikLeberg (niklaus.leuenb@gmail.com)
+ * @brief Platform implementation for "esp32"
+ * @version 0.1
+ * @date 2021-07-15
+ *
+ * @copyright Copyright (c) 2021 Niklaus Leuenberger
+ *
+ */
+
+
+/*
+ * Includes
+ *
+ */
+
+#include "platform.h"
+
+
+/*
+ * Type declarations
+ *
+ */
+
+/* ... */
+
+
+/*
+ * Variable declarations
+ *
+ */
+
+/* ... */
+
+
+/*
+ * Private function prototypes
+ *
+ */
+
+/* ... */
+
+
+/*
+ * Implementation of public functions
+ *
+ */
+
+/* ... */
+
+
+/*
+ * Implementation of private functions
+ *
+ */
+
+/**
+ * @brief esp-idf entry point.
+ *
+ * Somewhere in the FreeRTOS implementation of esp-idf this function gets called
+ * on the first xtensa-core. Simply call the main() of the main application.
+ *
+ */
+void app_main() {
+    // Call the main application.
+    main();
+}
---
+++ platform/platform.h
@@ -0,0 +1,72 @@
+/**
+ * @file platform.h
+ * @author NikLeberg (niklaus.leuenb@gmail.com)
+ * @brief Platform abstraction
+ * @version 0.1
+ * @date 2021-07-15
+ *
+ * @copyright Copyright (c) 2021 Niklaus Leuenberger
+ *
+ * The platform implementation may provide following POSIX-apis:
+ * - Standard Functions:
+ *      - <stdlib.h>
+ *      - <stdbool.h>
+ *      - <stddef.h>
+ *      - <stdint.h>
+ *      - <stdio.h>
+ *      - <string.h>
+ * - Threads & Semaphores:
+ *      - <pthread.h>
+ *      - <semaphore.h>
+ * - Berkley Sockets:
+ *      - <arpa/inet.h>
+ *      - <net/if.h>
+ *      - <netdb.h>
+ *      - <netinet/in.h>
+ *      - <netinet/tcp.h>
+ *
+ * Additionally GPIO and peripheral apis according to the definitions in this
+ * header may be provided.
+ *
+ * If a platform can't provide such functionality, the api has to be stubbed out
+ * and must return sensible errors or trigger an assert.
+ */
+
+#pragma once
+
+
+/*
+ * Includes
+ *
+ */
+
+/* ... */
+
+
+/*
+ * Type declarations
+ *
+ */
+
+/* ... */
+
+
+/*
+ * Variable declarations
+ *
+ */
+
+/* ... */
+
+
+/*
+ * Public functions
+ *
+ */
+
+/**
+ * @brief Main entrypoint of the application.
+ *
+ * @return int errorlevel
+ */
+int main();
