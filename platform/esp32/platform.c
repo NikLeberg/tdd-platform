b583073127c0412c4016979a6dc924556f36be4b
NikLeberg <niklaus.leuenb+github@gmail.com>
2021-09-30 23:26:15 +0000

    esp32: Platform implementation, untested

--- platform/esp32/platform.c
+++ platform/esp32/platform.c
@@ -15,8 +15,11 @@
  *
  */
 
+#include <errno.h>
+
 #include "platform.h"
 
+#include "driver/gpio.h"
 
 /*
  * Type declarations
@@ -47,7 +50,116 @@
  *
  */
 
-/* ... */
+int gpio_setupPin(int pin, gpio_dirMode_t dirMode, gpio_pullMode_t pullMode) {
+    if (0 > pin || GPIO_NUM_MAX <= pin) {
+        return ENOTSUP;
+    }
+    esp_err_t err = ESP_OK;
+    switch (dirMode) {
+    case (GPIO_DIRMODE_DONT_CHANGE):
+        break;
+    case (GPIO_DIRMODE_INPUT):
+        err = gpio_set_direction(pin, GPIO_MODE_INPUT);
+        break;
+    case (GPIO_DIRMODE_OUTPUT):
+        err = gpio_set_direction(pin, GPIO_MODE_OUTPUT);
+        break;
+    default:
+        return EINVAL;
+    }
+    if (ESP_OK != err) {
+        return EIO;
+    }
+    switch (pullMode) {
+    case (GPIO_PULLMODE_DONT_CHANGE):
+        break;
+    case (GPIO_PULLMODE_UP):
+        err = gpio_set_pull_mode(pin, GPIO_PULLUP_ONLY);
+        break;
+    case (GPIO_PULLMODE_DOWN):
+        err = gpio_set_pull_mode(pin, GPIO_PULLDOWN_ONLY);
+        break;
+    case (GPIO_PULLMODE_UP_DOWN):
+        err = gpio_set_pull_mode(pin, GPIO_PULLUP_PULLDOWN);
+        break;
+    case (GPIO_PULLMODE_FLOATING):
+        err = gpio_set_pull_mode(pin, GPIO_FLOATING);
+        break;
+    default:
+        return EINVAL;
+    }
+    if (ESP_OK != err) {
+        return EIO;
+    }
+}
+
+int gpio_getLevel(int pin) {
+    if (0 > pin || GPIO_NUM_MAX <= pin) {
+        return -ENOTSUP;
+    }
+    return gpio_get_level(pin);
+}
+
+int gpio_setLevel(int pin, int level) {
+    if (0 > pin || GPIO_NUM_MAX <= pin) {
+        return ENOTSUP;
+    }
+    if (ESP_OK != gpio_set_level(pin, level)) {
+        return EIO;
+    }
+    return 0;
+}
+
+int gpio_setupInterrupt(int pin, gpio_interruptTrigger_t trigger,
+                        gpio_interruptCallback_t callback, void *arg) {
+    if (0 > pin || GPIO_NUM_MAX <= pin) {
+        return ENOTSUP;
+    }
+    static int isrServiceInstalled = 0;
+    if (!isrServiceInstalled) {
+        if (ESP_OK != gpio_install_isr_service(0)) {
+            return EIO;
+        }
+        isrServiceInstalled = 1;
+    }
+    if (ESP_OK != gpio_isr_handler_add(pin, callback, arg)) {
+        return ENOTSUP;
+    }
+    gpio_int_type_t intrType;
+    switch (trigger) {
+    case (GPIO_INT_TRIGGER_DONT_CHANGE):
+        break;
+    case (GPIO_INT_TRIGGER_DISABLED):
+        intrType = GPIO_INTR_DISABLE;
+        break;
+    case (GPIO_INT_TRIGGER_POSITIVE_EDGE):
+        intrType = GPIO_INTR_POSEDGE;
+        break;
+    case (GPIO_INT_TRIGGER_NEGATIVE_EDGE):
+        intrType = GPIO_INTR_NEGEDGE;
+        break;
+    case (GPIO_INT_TRIGGER_ANY_EDGE):
+        intrType = GPIO_INTR_ANYEDGE;
+        break;
+    case (GPIO_INT_TRIGGER_LOW_LEVEL):
+        intrType = GPIO_INTR_LOW_LEVEL;
+        break;
+    case (GPIO_INT_TRIGGER_HIGH_LEVEL):
+        intrType = GPIO_INTR_HIGH_LEVEL;
+        break;
+    default:
+        return EINVAL;
+    }
+    if (GPIO_INT_TRIGGER_DONT_CHANGE != trigger) {
+        if (ESP_OK != gpio_set_intr_type(pin, intrType)) {
+            return EIO;
+        }
+    }
+    if (ESP_OK != gpio_intr_enable(pin)) {
+        return EIO;
+    }
+    return 0;
+}
 
 
 /*
--- platform/platform.h
+++ platform/platform.h
@@ -42,13 +42,52 @@
 
 /* ... */
 
-
 /*
  * Type declarations
  *
  */
 
-/* ... */
+/**
+ * @brief Direction modes for GPIO pin.
+ *
+ */
+typedef enum {
+    GPIO_DIRMODE_DONT_CHANGE, //!< Keep default or existing configuration.
+    GPIO_DIRMODE_INPUT,       //!< Pin is a input.
+    GPIO_DIRMODE_OUTPUT       //!< Pin is a output.
+} gpio_dirMode_t;
+
+/**
+ * @brief Pullup & -down modes for GPIO pin.
+ *
+ */
+typedef enum {
+    GPIO_PULLMODE_DONT_CHANGE, //!< Keep default or existing configuration.
+    GPIO_PULLMODE_UP,          //!< Enable only pullup resistor.
+    GPIO_PULLMODE_DOWN,        //!< Enable only pulldown resistor.
+    GPIO_PULLMODE_UP_DOWN,     //!< Enable pullup and -down resistor.
+    GPIO_PULLMODE_FLOATING     //!< Disable both pull resistors.
+} gpio_pullMode_t;
+
+/**
+ * @brief Type of triggers for gpio interrupts.
+ *
+ */
+typedef enum {
+    GPIO_INT_TRIGGER_DONT_CHANGE,   //!< Keep default or existing configuration.
+    GPIO_INT_TRIGGER_DISABLED,      //!< Disable interrupt.
+    GPIO_INT_TRIGGER_POSITIVE_EDGE, //!< On rising edge of signal level.
+    GPIO_INT_TRIGGER_NEGATIVE_EDGE, //!< On falling edge of signal level.
+    GPIO_INT_TRIGGER_ANY_EDGE,      //!< On all signal level changes.
+    GPIO_INT_TRIGGER_LOW_LEVEL,     //!< While signal is low. (continuous!)
+    GPIO_INT_TRIGGER_HIGH_LEVEL,    //!< While signal is high. (continuous!)
+} gpio_interruptTrigger_t;
+
+/**
+ * @brief Prototype of interrupt callback.
+ *
+ */
+typedef void (*gpio_interruptCallback_t)(void *);
 
 
 /*
@@ -63,6 +102,53 @@
  * Public functions
  *
  */
+
+/**
+ * @brief Setup common GPIO pin settings.
+ *
+ * @note Every setting accepts *_DONT_CHANGE whitch ignores the setting e.g.
+ * keeps the default or an already existing setting.
+ *
+ * @param pin Number of the GPIO pin to setup.
+ * @param dirMode Direction of dataflow.
+ * @param pullMode Pullup & -down resistors.
+ * @return int 0 on success EINVAL, ENOTSUP or EIO on error.
+ */
+int gpio_setupPin(int pin, gpio_dirMode_t dirMode, gpio_pullMode_t pullMode);
+
+/**
+ * @brief Get current level of GPIO pin.
+ *
+ * @param pin Number of the GPIO pin.
+ * @return int 0 = Low, 1 = High, -EIO = error
+ */
+int gpio_getLevel(int pin);
+
+/**
+ * @brief Set level of GPIO pin.
+ *
+ * @param pin Number of the GPIO pin.
+ * @param level Level to set: 0 = Low, 1 = High
+ * @return int 0 on success or EIO on error.
+ */
+int gpio_setLevel(int pin, int level);
+
+/**
+ * @brief Setup a callback that gets called on the configured trigger.
+ *
+ * @warning Not all pins can be configured for interrupts.
+ * @note To disable interrupts on pin, call with \arg trigger = \ref
+ * GPIO_INT_TRIGGER_DISABLED.
+ *
+ * @param pin Number of the GPIO pin that triggers the interrupt.
+ * @param trigger Trigger for the interrupt.
+ * @param callback Callback function that is executed on interrupt.
+ * @param arg Optional argument for callback.
+ * @return int 0 on success, EIO on I/O error or ENOTSUP if pin does not
+ * support interrupts.
+ */
+int gpio_setupInterrupt(int pin, gpio_interruptTrigger_t trigger,
+                        gpio_interruptCallback_t callback, void *arg);
 
 /**
  * @brief Main entrypoint of the application.
