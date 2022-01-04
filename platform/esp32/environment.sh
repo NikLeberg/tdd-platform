45fd01db248f0d8e8622ad76bf9fa25ba54f14c6
NikLeberg <niklaus.leuenb+github@gmail.com>
2022-01-04 16:40:44 +0000

    esp: remove rfc2217 bridge definitions

--- platform/esp32/environment.sh
+++ platform/esp32/environment.sh
@@ -1,8 +1,2 @@
 # Source the normal esp-idf export.sh to have all the common tools on $PATH.
-source /root/.espressif/esp-idf/export.sh >/dev/nul
-# Set the port where the esp32 is available. The real port of the Windows host
-# is bridged to a socket on localhost. Obtain the ip of the host (as we are
-# inside a container) and use this as rfc2217 telnet port. All esp-idf tools
-# look for the following environment variable.
-# https://stackoverflow.com/questions/22944631/how-to-get-the-ip-address-of-the-docker-host-from-inside-a-docker-container
-export ESPPORT=rfc2217://host.docker.internal:2217
+. /root/.espressif/esp-idf/export.sh >/dev/nul
