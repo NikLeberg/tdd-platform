ed8766259a4fa2d2fd026fe26b41a2ffc69b4383
NikLeberg <niklaus.leuenb+github@gmail.com>
2022-03-01 13:46:08 +0000

    lib: add mongoose embedded webserver

---
+++ lib/mongoose/mongoose_custom.h
@@ -0,0 +1,51 @@
+/**
+ * @file mongoose_custom.h
+ * @author NikLeberg (niklaus.leuenb@gmail.com)
+ * @brief Custom header for mongoose
+ * @version 0.1
+ * @date 2022-03-01
+ *
+ * @copyright Copyright (c) 2022 Niklaus Leuenberger
+ *
+ * Includes the neccessary headers that mongoose needs.
+ */
+
+#pragma once
+
+
+/*
+ * Includes
+ *
+ */
+
+#include <ctype.h>
+#include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#include "platform/platform.h"
+
+
+/*
+ * Type declarations
+ *
+ */
+
+/* ... */
+
+
+/*
+ * Variable declarations
+ *
+ */
+
+/* ... */
+
+
+/*
+ * Public functions
+ *
+ */
+
+/* ... */
