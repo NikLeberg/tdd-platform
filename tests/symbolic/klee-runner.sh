b5c3f22427252965403f9b55442ce1be182f4c70
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2022-06-16 22:01:33 +0000

    symbolic: print out ktest file of failing tests

--- tests/symbolic/klee-runner.sh
+++ tests/symbolic/klee-runner.sh
@@ -1,17 +1,30 @@
 #!/bin/env bash
 
-# This script intends to continuously provide stats while running klee.
+# This script intends to continuously provide stats while running klee. And
+# after running analyzing the produced tests that crashed the program.
 # 
 # Args:
 # 1 - klee executable
 # 2 - klee-stats executable
-# 3 - llvm bytecode to execute
+# 3 - ktest-tool executable
+# 4 - llvm bytecode to execute
 
-$1 --entry-point=application_main --only-output-states-covering-new --optimize $3 &
+# Run klee itself in the background.
+$1 --entry-point=application_main --exit-on-error \
+    --only-output-states-covering-new --optimize $4 &
 PROC_ID=$!
 
+# As long as klee is running, print stats every 5 seconds.
 while kill -0 "$PROC_ID" >/dev/null 2>&1; do
     sleep 5
     $2 --print-more ./klee-last
 done
+
+# Print out ktest content for failing tests.
+files=($(ls ./klee-last/*err))
+for file in "${files[@]}" ; do
+    # Remove file extension of error message file and run through ktest tool.
+    $3 ${file%.*.*}.ktest
+done
+
 exit 0
